fe8b4ac827e4a2dabee1b3c5daf52e96b39b255a
Brande Gilbert <brandegilbert@gmail.com>
2021-08-06 09:09:57 -0400

    Final commit

--- week1/day4/todoApp.py
+++ week1/day4/todoApp.py
@@ -1,19 +1,3 @@
-#toDoList = {}
-#userAns = input("""
- #Press 1 to add task
- #Press 2 to delete task
- #Press 3 to view all tasks
- #Press q to quit
- # """)
-#while userAns == "1, 2, 3, q":
- #   if userAns == "1":
-  #      addTask = input("What task would you like to add?")
- #       addPriority = input("What is the priority, high, medium, low?")
-  #      print(addTask, addPriority)
-  #toDoItem = addTask + addPriority
-
-
-
 userAns = ""
 toDoList =[]
 while userAns != "q":
@@ -24,7 +8,7 @@ while userAns != "q":
 
     userAns = input("\nWhat would you like to do? ")
     if userAns == "1":
-        addTask = input("\nWhat is the task ")
+        addTask = input("\nWhat is the task? ")
         addPriority = input("\nWhat is the priority, high, medium, or low? ")
         print(addTask + " - " + addPriority)
         toDoList.append(addTask + " - " + addPriority)
@@ -45,7 +29,6 @@ while userAns != "q":
        for todo in toDoList:
            print("%d - %s" % (count, todo))
            count += 1
-       
-        #Show user all the tasks along with the index number of each task. User can then enter the index number of the task to delete the task.
-
-        #see if github works
+    elif userAns == "q":
+        print("Thank you for using the To Do App. Have a productive day!")
+        break
