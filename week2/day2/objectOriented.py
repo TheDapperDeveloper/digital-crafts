3793d028659ee73b5868d62b2b18fdb946b586d0
Brande Gilbert <brandegilbert@gmail.com>
2021-08-10 11:16:23 -0400

    Update

---
+++ week2/day2/objectOriented.py
@@ -0,0 +1,9 @@
+# Intro to classes
+
+# Class: a blueprint from which you can make objects: 
+# can include variables/attributes and functions/methods
+
+#List ["can", 123, {}]
+# Dictionary {"key", "value"}
+# Tuple (12341234, 4564564)
+
