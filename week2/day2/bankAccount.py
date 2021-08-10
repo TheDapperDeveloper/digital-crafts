e62ccbebc3118c3d581bb95f61d30ec3e56b21e9
Brande Gilbert <brandegilbert@gmail.com>
2021-08-10 16:44:06 -0400

    update

--- week2/day2/bankAccount.py
+++ week2/day2/bankAccount.py
@@ -6,9 +6,15 @@ class BankAccount:
         self.balance += add_funds
     def withdraw(self, remove_funds):
         self.balance -= remove_funds
-    def transfer_funds(self, from_account, to_account):
-        self.balance -= from_account
-        self.balance += to_account
+    def transfer_funds(self, balance, account_number, from_checking_account, to_savings_account):
+        super().__init__(balance, account_number)
+        self.balance -= from_checking_account
+        self.balance += to_savings_account
 
-checking_account = BankAccount(1000, 298502387)
-print(checking_account)
+checking_account = BankAccount(1000, "298502387")
+checking_account.deposit(5000)
+print(checking_account.balance)
+
+checking_account = BankAccount(1000, "1234848")
+checking_account.transfer_funds(1000, "123456", 500, 500)
+print(checking_account.balance)
