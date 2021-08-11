29cb8f0e6f6f1ea91092dbb32592db72c2390ab1
Brande Gilbert <brandegilbert@gmail.com>
2021-08-11 12:02:09 -0400

    Update

--- week2/day2/groceryApp.py
+++ week2/day2/groceryApp.py
@@ -1,22 +1,4 @@
-userAns = ""
-shoppingList =[]
-
-print("Press 1 to create a Shopping List")
-print("Press 2 to add to an Existing Shopping List")
-print("Press D to view all Shopping Lists")
-print("Press q to quit")
-        
-userAns = input("\nWhat would you like to do? ")
-if userAns == "1":
-        addList = input("\nWhat is the name of the store? ")
-        addAddress = input("\nWhat is the address? ")
-        print(addList + " - " + addAddress)
-        shoppingList.append(addList + " - " + addAddress)
-        listDict = {}
-        listDict = shoppingList
-        print(shoppingList)
-elif userAns == "2":
-        addToExisting = input #How do I call addItems function below?
+shoppingList = []
 
 class GroceryItems:
     def __init__ (self, title, price, quantity):
@@ -24,8 +6,41 @@ class GroceryItems:
         self.price = price
         self.quantity = quantity
         self.groceryList = []
-    def addItems (self, title):
-        self.groceryList.append(title)
-    def displayLists(self):
-        print(self.groceryList)
+    def thingsToBuy(self):
+        print("Item: " + self.title + "Price:$ " + self.price + "Quantity: " + self.quantity)
+
+       
+def newItems():
+    newList = input("\n What is the name and address of the store?")
+    shoppingList.append(newList)
+    print(shoppingList)
+    
+def addItems():
+    count = 1
+    for items in shoppingList:
+        print("%d: %s" % (count,items))
+        count +=1
+    addList = input("\nWhich list would you like to add to?")
+    shoppingList.append(addList)
+
+
+
+userAns = ""
+while userAns != "q":
+    print(""" Welcome to the shopping list app.
+    Press 1 to create a new shopping list.
+    Press 2 to add to an existing shopping list.
+    Press 3 to view all shopping lists.
+    Press q to quit. """)
+    userAns = input("\nWhat would you like to do? ")
+    if userAns == "1":
+        newItems()
+    elif userAns == "2":
+        addItems()
+        food = input("Enter title ", "price ", "and quantity")
+        food.thingsToBuy()
+
+
+
+       
 
