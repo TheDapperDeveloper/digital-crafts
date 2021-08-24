0c3f2e0fcfc201ae9be05fd064950947bcfb01ca
Brande Gilbert <brandegilbert@gmail.com>
2021-08-24 15:22:41 -0400

    Update

--- week4/day2/todo.js
+++ week4/day2/todo.js
@@ -1,16 +1,19 @@
-const container = document.querySelector(".main-container")
-const newContainer = document.querySelector(".sec-container");
-const input = document.querySelector(".input");
+const container = document.querySelector(".main-container");
+const input = document.getElementById("userList");
 const submitButton = document.querySelector(".submit-button");
+const newContainer = document.querySelector(".sec-container");
 
-const listItems = document.createElement("li");
-
+//const listItems = document.createElement("li");
 
 function itemAdded() {
+    const listItems = document.createElement("li");
     const toDoEntry = input.value;
     listItems.innerHTML = toDoEntry;
     newContainer.append(toDoEntry);
     console.log("Here is the item", toDoEntry);
 }
 
-submitButton.onclick = () => itemAdded();
+submitButton.addEventListener("click", () => {
+    itemAdded();
+})
+
