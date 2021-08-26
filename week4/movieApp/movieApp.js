4150d5937eae0ebb9941c8558f78e8d209ddd3f5
Brande Gilbert <brandegilbert@gmail.com>
2021-08-26 11:25:41 -0400

    Update

--- week4/movieApp/movieApp.js
+++ week4/movieApp/movieApp.js
@@ -1,10 +1,9 @@
-const movieContainer = document.querySelector(".main-container");
-const cardContainer = document.querySelector(".movie-cards");
-const input = document.getElementById("search-field").value;
+//const movieContainer = document.querySelector(".main-container");
+
 const searchButton = document.querySelector(".search-button")
 const searchURL = "http://www.omdbapi.com/"
 const apiKey = "?apikey=f0d4af81"
-//movieContainer.innerHTML = ""
+
 
 async function searchMovie () {
     const cardContainer = document.querySelector(".movie-cards");
@@ -12,13 +11,25 @@ async function searchMovie () {
     const moviePosterPics = await fetch(`${searchURL}` + apiKey + "&s=" + input) 
     console.log(`${searchURL}` + apiKey + "&s="+ input)
     const jsonMovie = await moviePosterPics.json();
+    
+    for (const singleMovieData of jsonMovie.Search) {
+        //Do I need to create a new div?
+        const movieTitles = document.createElement("h3")
+        movieTitles.className = "posters"
+        movieTitles.innerHTML = singleMovieData.Title
+        cardContainer.append(movieTitles)
+        console.log(movieTitles)
+    }
+    
+    
     for (const movieInObject of jsonMovie.Search) {
-        //input.innerText = ""
         const convertedPics = document.createElement("img")
         convertedPics.src = movieInObject.Poster
        cardContainer.append(convertedPics)
        console.log(movieInObject.Poster)
+       
     }
+
 }
     
 searchButton.addEventListener("click", () => searchMovie())
