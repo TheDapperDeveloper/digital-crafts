e28a446377f3262f601a61b861d0046194596616
Brande Gilbert <brandegilbert@gmail.com>
2021-08-27 12:10:42 -0400

    Update

--- week4/day5/api.js
+++ week4/day5/api.js
@@ -1,14 +1,22 @@
 const searchButton = document.querySelector(".button")
-const dataURL = "api.openweathermap.org/data/2.5/weather?q="
+const dataURL = "https://api.openweathermap.org/data/2.5/weather?q="
 const apiKey= "622d85fb1a656b0a3ab04b6a4f7fe706"
 
 async function weatherSearch () {
     const weatherContainer = document.createElement("div");
     weatherContainer.className = "weather-container"
     const input = document.querySelector(".name-of-city").value;
-    const currentWeather = await fetch (`${dataURL}`+ input + "&appid" + apiKey)
-    console.log(`${dataURL}`+ input + "&appid" + apiKey)
+    const currentWeather = await fetch (`${dataURL}`+ input + "&units=imperial&appid=" + apiKey)
+    console.table(`${dataURL}`+ input + "&units=imperial&appid=" + apiKey)
     const convertedWeather = await currentWeather.json();
+
+    for (const dailyWeather of convertedWeather) {
+        const weatherContainer = document.querySelector(".weather-container")
+        const weatherText = document.createElement("h1")
+        console.log(weatherText)
+        weatherText.innerHTML = dailyWeather.temp
+        weatherContainer.append(weatherText)
+    }
 
 }
 
