6ecf7bfc6da5e6923068f01fc5fa5efabdd9363c
Brande Gilbert <brandegilbert@gmail.com>
2021-08-29 16:44:08 -0400

    If statement complete

--- week5/api.js
+++ week5/api.js
@@ -5,20 +5,22 @@ const apiKey = "VAzKyUZMmI54bec3AdGCuviya3Z8IEF1"
 async function searchBook () {
     const mainContainer = document.querySelector(".main-container")
     const infoContainer = document.querySelector(".info-container")
+    infoContainer.innerHTML = ''
     const input = document.querySelector(".input").value;
+    
     const searchBook = await fetch(`${searchURL}` + "title=" + input + "&api-key=" + apiKey)
     const jsonBook = await searchBook.json();
     console.table(jsonBook)
+
 // need to add for loop in case there are multiple reviews
-// need to add if statement if there is no review
-    console.log(jsonBook.num_results)
-    if (jsonBook.num_results = 0) {
+
+    if (jsonBook.num_results <= '0') {
       const noReview = document.createElement("h3")
        noReview.innerHTML = "The New York Times did not publish a review for this book."
         console.log(noReview)
        mainContainer.append(infoContainer)
         infoContainer.append(noReview)
-    } else if (jsonBook.num_results > 0) {
+    } else if (jsonBook.num_results > '0') {
         const bookTitle =jsonBook.results[0].book_title
         const viewTitle = document.createElement("h3")
         viewTitle.innerHTML = bookTitle
@@ -44,6 +46,8 @@ async function searchBook () {
 
     const bookDate =jsonBook.results[0].publication_dt
     console.log(bookDate)
+
+    
     }
 }
 
