71b81ff9e5c88908b2e2642b5aebc6bf09c7f49e
Brande Gilbert <brandegilbert@gmail.com>
2021-08-29 10:18:27 -0400

    api complete. need to style

--- week5/api.js
+++ week5/api.js
@@ -4,24 +4,33 @@ const apiKey = "VAzKyUZMmI54bec3AdGCuviya3Z8IEF1"
 
 async function searchBook () {
     const mainContainer = document.querySelector(".main-container")
+    const infoContainer = document.querySelector(".info-container")
     const input = document.querySelector(".input").value;
     const searchBook = await fetch(`${searchURL}` + "title=" + input + "&api-key=" + apiKey)
-    console.table(`${searchURL}` + "title=" + input + "&api-key=" + apiKey)
     const jsonBook = await searchBook.json();
-    console.log(jsonBook)
-
-    const bookReview = jsonBook.results[0].url
-    console.table(bookReview)
-    const reviewURL = document.createElement("link")
-    reviewURL.href = bookReview
-    console.log(reviewURL)
-    mainContainer.append(reviewURL)
 
     const bookTitle =jsonBook.results[0].book_title
-    console.log(bookTitle)
+    const viewTitle = document.createElement("h3")
+    viewTitle.innerHTML = bookTitle
+    mainContainer.append(infoContainer)
+    infoContainer.append(bookTitle)
+    
 
     const bookAuthor = jsonBook.results[0].book_author
-    console.log(bookAuthor)
+    const viewAuthor = document.createElement("h3")
+    viewAuthor.innerHTL = bookAuthor
+    mainContainer.append(infoContainer)
+    infoContainer.append(bookAuthor)
+
+    const bookReview = jsonBook.results[0].url
+    const reviewURL = document.createElement("a")
+    reviewURL.href = bookReview
+    reviewURL.innerHTML = "View Book Review"
+    mainContainer.append(infoContainer)
+    infoContainer.append(reviewURL)
+
+    
+
 
     const bookDate =jsonBook.results[0].publication_dt
     console.log(bookDate)
