d961803773c076978d1a8c25c6a4f219cf35ea5c
Brande Gilbert <brandegilbert@gmail.com>
2021-08-29 07:47:29 -0400

    link to article complete for api

--- week5/api.js
+++ week5/api.js
@@ -5,10 +5,12 @@ const apiKey = "VAzKyUZMmI54bec3AdGCuviya3Z8IEF1"
 async function searchBook () {
     const mainContainer = document.querySelector(".main-container")
     const input = document.querySelector(".input").value;
-    const searchBook = await fetch(`${searchURL}` + "title=" + input + "&" + apiKey)
+    const searchBook = await fetch(`${searchURL}` + "title=" + input + "&api-key=" + apiKey)
     console.table(`${searchURL}` + "title=" + input + "&api-key=" + apiKey)
     const jsonBook = await searchBook.json();
     console.log(jsonBook)
+    const bookReview = jsonBook.results[0].url
+    console.table(bookReview)
 }
 
 searchButton.addEventListener("click", () => searchBook())
