eb32b4203f178773e774773d8748dbcb5cfde015
Brande Gilbert <brandegilbert@gmail.com>
2021-09-01 14:35:03 -0400

    Update to style

--- week5/api.js
+++ week5/api.js
@@ -3,10 +3,12 @@ const searchURL = "https://api.nytimes.com/svc/books/v3/reviews.json?"
 const apiKey = "VAzKyUZMmI54bec3AdGCuviya3Z8IEF1"
 
 async function searchBook () {
+    
     const mainContainer = document.querySelector(".main-container")
     const infoContainer = document.querySelector(".info-container")
-    infoContainer.innerHTML = ''
+    
     const input = document.querySelector(".input").value;
+    
     
     const searchBook = await fetch(`${searchURL}` + "title=" + input + "&api-key=" + apiKey)
     const jsonBook = await searchBook.json();
@@ -25,25 +27,27 @@ async function searchBook () {
                         const viewTitle = document.createElement("h1")
                           viewTitle.innerHTML = books.book_title
                           const bookAuthorName = document.createElement("h2")
-                          bookAuthorName.innerHTML = books.book_author
+                          bookAuthorName.innerHTML = "Author: " + books.book_author
                           const articleAuthorName = document.createElement("h3")
-                          const viewAuthor = books.byline
+                          const viewAuthor = "Review written by " + books.byline
                           articleAuthorName.innerHTML = viewAuthor
                           mainContainer.append(infoContainer)
                           infoContainer.append(viewTitle, bookAuthorName, articleAuthorName)
 
-                        const bookReview = books.url
-                        const reviewURL = document.createElement("a")
-                        reviewURL.href = bookReview
-                        reviewURL.innerHTML = "View Book Review"
-                        mainContainer.append(infoContainer)
-                        infoContainer.append(reviewURL)
 
                         const summary = books.summary
                         const viewSummary = document.createElement("h3")
                         viewSummary.innerHTML = summary
                         mainContainer.append(infoContainer)
                         infoContainer.append(viewSummary)
+
+                        const bookReview = books.url
+                        const reviewURL = document.createElement("a")
+                        reviewURL.href = bookReview
+                        reviewURL.innerHTML = "Click here to read the review."
+                        mainContainer.append(infoContainer)
+                        infoContainer.append(reviewURL)    
+                        
                         
                   }
 
