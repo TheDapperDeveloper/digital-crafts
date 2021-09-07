28ed46ef613525bf75ac16e13aaebffe23282a92
Brande Gilbert <brandegilbert@gmail.com>
2021-09-07 15:20:34 -0400

    update

--- week6/day1/practice.js
+++ week6/day1/practice.js
@@ -1,20 +1,42 @@
+// Hello World Servor and Add Routes
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
+// app.get("/", (req, res) => {
+//     res.send("Hello World!")
+// });
+
+// app.post("/cats", (req, res) => {
+//     res.send("Meow!")
+// });
+
+// app.put("/dogs", (req, res) => {
+//     res.send("Woof!")
+// });
+
+// app.patch("/cats_and_dogs", (req, res)=> {
+//     res.send("Dogs and cats living together...mass hysteria!!")
+// })
+
+// app.listen(3000, console.log("I'm on port 3000"))
+
+//Add Routes with Parameters
+
+const params = [
+    {
+        name: "Luke"
+    },
+    {
+        name: "Ashoka"
+    },
+    {
+        name: "Han"
+    },
+];
+
+app.get("/greet/:name", (req, res) => {
+    const { name } = req.params
+    res.send(`Hello, ${name}!`);
 });
 
-app.post("/cats", (req, res) => {
-    res.send("Meow!")
-});
-
-app.put("/dogs", (req, res) => {
-    res.send("Woof!")
-});
-
-app.patch("/cats_and_dogs", (req, res)=> {
-    res.send("Dogs and cats living together...mass hysteria!!")
-})
-
-app.listen(3000, console.log("I'm on port 3000"))
+app.listen(3000, console.log("This works"))
