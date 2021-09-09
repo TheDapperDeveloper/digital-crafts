1931798ff7ba39a0db8274087029416f466f3207
Brande Gilbert <brandegilbert@gmail.com>
2021-09-09 16:23:10 -0400

    Update

--- week6/day3/homework.js
+++ week6/day3/homework.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const express = require("express") //S
-const app = express() //S
+const express = require("express") 
+const app = express() 
 const token = process.env.API_KEY;
 const URL = process.env.URL;
 
@@ -9,14 +9,16 @@ const supabase = createClient(URL, token)
 
 app.use(express.json());
 
-const PORT = 3000 //S
+const PORT = 3000 
 
+//Route to add stock
 app.post("/newStock", async (req, res) => {
     const { data, error } = await supabase.from("stockInfo").insert(req.body)
     console.log(req.body)
     res.send(data)
   });
 
+  // Route to delete stock
 app.delete("/deleteStock/:id", async (req, res) => {
     const id = req.params.id;
     const { data, error } = await supabase
@@ -26,6 +28,7 @@ app.delete("/deleteStock/:id", async (req, res) => {
     res.send(data)
 });
 
+// Route to update individual stock
 app.put("/updateStock/:id", async (req, res) => {
     const id = req.params.id
     const { data, error } = await supabase
@@ -36,12 +39,22 @@ app.put("/updateStock/:id", async (req, res) => {
 
 })
 
+// Route to update many
+app.put("/updateMany/:", async (req, res) => {
+    const { data, error } = await supabase
+    .from("stockInfo")
+    .upsert({data})
+})
+
+//Route to view stock info
 app.get("/viewStock", async (req, res) => {
     const { data, error } = await supabase
     .from("stockInfo")
     .select()
     res.send(data)
 })
+
+//Route to view specific stock
 
 app.get("/viewStockById/:id", async (req, res) => {
     console.log()
@@ -52,4 +65,4 @@ app.get("/viewStockById/:id", async (req, res) => {
     res.send(data)
 })
 
-app.listen(PORT, console.log(`Listening on ${PORT}`)) //S
+app.listen(PORT, console.log(`Listening on ${PORT}`)) 
