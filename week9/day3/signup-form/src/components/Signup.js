0f071c423d4fb2b06cacd63e4c6561107ea996e0
Brande Gilbert <brandegilbert@gmail.com>
2021-09-29 16:18:03 -0400

    Update to setstate

--- week9/day3/signup-form/src/components/Signup.js
+++ week9/day3/signup-form/src/components/Signup.js
@@ -8,14 +8,14 @@ function Signup (props) {
             <h1>Sign Up</h1>
             <form action="">
             <input onChange={(e) => props.firstName({firstName: e.target.value})}  type="text" name="firstName" placeholder="First Name"  />
-            <input onChange={(e) => props.lastName({firstName: e.target.value})} type="date" name="dob" placeholder="Date of Birth" />
-            <input onChange={(e) => props.dob({firstName: e.target.value})} type="text" name="lastName" placeholder="Last Name" />
-            <input onChange={(e) => props.address({firstName: e.target.value})} type="text" name="address" placeholder="Address" />
-            <input onChange={(e) => props.city({firstName: e.target.value})} type="text" name="city" placeholder="City"  />
-            <input onChange={(e) => props.state({firstName: e.target.value})} type="text" name="state" placeholder="State" />
-            <input onChange={(e) => props.zipcode({firstName: e.target.value})} type="text" name="zipcode" placeholder="Zip Code" />
-            <input onChange={(e) => props.username({firstName: e.target.value})} type="text" name="username" placeholder="Username" />
-            <input onChange={(e) => props.email({firstName: e.target.value})} type="text" name="email" placeholder="E-mail" />
+            <input onChange={(e) => props.lastName({lastName: e.target.value})} type="text" name="lastName" placeholder="Last Name" />
+            <input onChange={(e) => props.dob({dob: e.target.value})} type="date" name="dob" placeholder="Date of Birth" />
+            <input onChange={(e) => props.address({address: e.target.value})} type="text" name="address" placeholder="Address" />
+            <input onChange={(e) => props.city({city: e.target.value})} type="text" name="city" placeholder="City"  />
+            <input onChange={(e) => props.state({state: e.target.value})} type="text" name="state" placeholder="State" />
+            <input onChange={(e) => props.zipcode({zipcode: e.target.value})} type="text" name="zipcode" placeholder="Zip Code" />
+            <input onChange={(e) => props.username({username: e.target.value})} type="text" name="username" placeholder="Username" />
+            <input onChange={(e) => props.email({email: e.target.value})} type="text" name="email" placeholder="E-mail" />
             {/* <input onClick={() => SubmitButton(firstName)} type="button" value="Submit" /> */}
             </form>
         </div>
