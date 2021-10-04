dab23ff9ba24af7d80e6275257c05813eae520cd
Brande Gilbert <brandegilbert@gmail.com>
2021-10-04 16:06:44 -0400

    Update

--- week9/day4/form-validations-2/src/components/SidebarLinks.js
+++ week9/day4/form-validations-2/src/components/SidebarLinks.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { SidebarLink } from '../styled-components/SidebarStyle'
 export default function SidebarLinks(props) {
-    console.log(props.linkData.path)
-    console.log(props.linkData.name)   
+      
     return (
         <>
          <SidebarLink to={props.linkData.path}>{props.linkData.name}</SidebarLink>   
