915f4af8c16764781b35871fa8a9de66e0e243e1
Brande Gilbert <brandegilbert@gmail.com>
2021-10-06 11:15:02 -0400

    Hooked up redux to application

--- week9/day4/form-validations-2/src/index.js
+++ week9/day4/form-validations-2/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import {rootReducer} from "./reducers/rootReducer"
 
+
+
+const store = createStore(rootReducer, applyMiddleware(logger)); 
 
 ReactDOM.render(
+  
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
