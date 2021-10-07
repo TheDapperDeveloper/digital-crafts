31ee9c1fec5bc3a3a9117b97c452d9c8b2ce61b4
Brande Gilbert <brandegilbert@gmail.com>
2021-10-07 15:24:29 -0400

    Chart complete

--- week9/day4/form-validations-2/src/components/Chart.js
+++ week9/day4/form-validations-2/src/components/Chart.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import { ChartBorder } from '../styled-components/ChartStyle'
+import Plot from 'react-plotly.js'
 
 export default function Chart() {
+
+    const exoticCars = {
+        x: ['Lamborghini', 'Rolls Royce', 'Aston Martin'],
+        y: [20, 14, 23],
+        name: 'Exotic Cars',
+        type: 'bar'
+      };
+      
+     const luxuryWatches = {
+        x: ['Audemars Piquet', 'Patek Phillipe', 'Hublot'],
+        y: [12, 18, 29],
+        name: 'Luxury Watches',
+        type: 'bar'
+      };
+
+      const data = [exoticCars, luxuryWatches];
+
+      const layout = {barmode: 'group'};
+
     return (
         <ChartBorder>
+            <Plot data={data}
+            layout={{title: "Brande's Car and Watch Inventory"}}
+            look={layout}
+            />
             
         </ChartBorder>
     )
--- week9/day4/form-validations-2/src/styled-components/ChartStyle.js
+++ week9/day4/form-validations-2/src/styled-components/ChartStyle.js
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 
 export const ChartBorder = styled.div`
+    display: flex;
+    justify-content: center;
    border: 2px solid black;
    height: 450px;
    width: 1000px;
